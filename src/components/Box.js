14a556e36d896b718de1e0e171128358c5723a61
ivyhy98 <ivanahy98@gmail.com>
2022-12-05 20:54:25 -0500

    finished file structure updated css

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,7 +3,6 @@ export default function Box(props){
         <div className="box">
             <h3>{props.title}</h3>
             <h2 className="subtitle">{props.result}</h2>
-
         </div>
     );
 };
